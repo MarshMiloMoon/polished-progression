8fa9fb164f4ee8d3b556e0c1bd0ef82029f804b9
MarshMiloMoon <milomoon06@gmail.com>
2024-09-14 23:03:53 -0700

    implemented data gen (removed all now obsolete json files), removed vanilla stone tool recipes.

---
+++ src/main/java/net/starglobe/polishedprogression/util/ModLootTableModifiers.java
@@ -0,0 +1,18 @@
+package net.starglobe.polishedprogression.util;
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ModLootTableModifiers {
+    public static final Identifier SUSPICIOUS_SAND_ID =
+            Identifier.of("minecraft", "archaeology/desert_pyramid");
+
+    public static void modifyLootTables() {
+
+    }
+}
